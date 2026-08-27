b6f6a4d605380fdd85114ef0fe5fdaaf120b4301
casualshop95 <casualshop95@gmail.com>
2026-08-27 22:05:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,10 +50,8 @@ app.get('/', (req, res) => {
   res.send('MU-MU GRILL order server is running.');
 });
 
+// Запускаємо сервер ТІЛЬКИ ОДИН РАЗ на порту з process.env.PORT або 3000
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
